13b055d582825356b486fcdb544917c53de843f9
dfnbfdgn <159501369+dfnbfdgn@users.noreply.github.com>
2024-03-15 15:37:40 -0300

    Updated Country
    Finished writing template for Country class

--- Country.java
+++ Country.java
@@ -1,4 +1,58 @@
 public class Country{
+  private String currencyName;
+  private double conversion
+  private weatherIntegration weather
+  private double temperature;
+
+  public Country(String currencyName, double conversion, waetherIntegration weather, double temperature){
+      this.currencyName = currencyName;
+      this.conversion = conversion
+      this.weather = weather;
+      this.temperature = temperature
+  }
+
+  // Get methods
+
+   public String getCurName(){
+		      return currencyName;
+	  }
+
+   public String getConver(){
+		      return conversion;
+	  }
+
+   public String getWeather(){
+		      return weather;
+	  }
+
+   public String getTemp(){
+		      return temperature;
+	  }
+
+   public void setCurName(String newName){
+		      currencyName = newName;
+	  }
+
+   public void setConver(double newConver){
+		      conversion = newConver;
+	  }
+
+   public void setWeather(weatherIntegration newWeather){
+		      weather = newWeather;
+	  }
+
+   public void setTemp(double newTemp){
+		      temperature = newTemp;
+	  }
+
+    public void display() {
+    			  System.out.println("Currency Name: " + currencyName);
+    			  System.out.println("Conversion Rate: " + conversion);
+    			  System.out.println("Weather: " + weather);
+    			  System.out.println("Temperature: " + temperature);
+    		  }
 
 
+
+  
 }
