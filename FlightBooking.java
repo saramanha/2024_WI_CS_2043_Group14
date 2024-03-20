2db6760e9c2f6962944260e22b054586b6f51c7c
dfnbfdgn <159501369+dfnbfdgn@users.noreply.github.com>
2024-03-20 17:12:42 -0300

    Create FlightBooking.java

---
+++ FlightBooking.java
@@ -0,0 +1,19 @@
+public class FlightBooking{
+  private customer passenger;
+  private flight[] bestFlight;
+
+  public FlightBooking(customer passenger, flight bestFlight){
+    passenger.this = passenger;
+    bestFlight.this = bestFlight;
+  }
+
+public flight[] findBestFlight(){
+
+}
+
+public void bookFlight(){
+  
+}
+
+
+}
