51cc51a8ff8b487c968fb578a21bcac647d05bc2
Ethan McCarthy <ethan.mccarthy@unb.ca>
2024-03-30 14:30:28 -0300

    Update FlightBooking.java

--- FlightBooking.java
+++ FlightBooking.java
@@ -77,7 +77,7 @@ public class FlightBooking {
    */
   public void printFlights(Flight[] arr){
       for(int i = 0; i < arr.length; i++){
-          System.out.println("Flight " + i + ": " + arr[i].getName());
+          System.out.println("Flight " + i + ": " + arr[i].getName() + " to " + arr[i].getDestination());
         }
     }
 
