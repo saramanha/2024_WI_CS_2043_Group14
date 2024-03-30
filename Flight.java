4e7caf5ea160b11ca4da7a416ce21b75d16ba466
dfnbfdgn <159501369+dfnbfdgn@users.noreply.github.com>
2024-03-30 14:13:40 -0300

    Update Flight.java

--- Flight.java
+++ Flight.java
@@ -6,6 +6,7 @@ public class Flight {
 	  private String flightName;
 	  private String insurance;    //change to insurance object soon
 	  private Date startDate;
+	  private String weather;
 
 	  //class method
 	  public Flight(Country countryIn, double costIn, String nameIn, String insuranceIn, Date startIn){
@@ -14,7 +15,7 @@ public class Flight {
 	    flightName = nameIn;
 	    insurance = insuranceIn;
 	    startDate = startIn;
-
+	    this.weather = null;
 	  }
 
 	  //get methods
