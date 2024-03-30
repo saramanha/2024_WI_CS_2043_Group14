c020fbf53a331640a11514dc4730993f1fe1b062
MeIIoreyes <160442233+MeIIoreyes@users.noreply.github.com>
2024-03-30 15:20:57 -0300

    Update Date.java with comments

--- Date.java
+++ Date.java
@@ -15,31 +15,58 @@ public class Date {
 	
 	//other methods
 
+	/**
+	 * The method returns the date's year.
+	 * @return The date's year.
+	 */
 	public int getYear() {
 		return year;
 	}
-	
+
+	/**
+	 * The method returns the date's month.
+	 * @return The date's month.
+	 */
 	public int getMonth() {
 		return month;
 	}
-	
+
+	/**
+	 * The method returns the date's day.
+	 * @return The date's day.
+	 */
 	public int getDay() {
 		return day;
 	}
-
+	
+	/**
+	 * The method sets the date's year.
+	 * @param The date's new year.
+	 */
 	public void setYear(int yearIn) {
 		year = yearIn;
 	}
-	
+
+	/**
+	 * The method sets the date's month.
+	 * @param The date's new month.
+	 */
 	public void setMonth(int monthIn) {
 		month = monthIn;
 	}
 	
+	/**
+	 * The method sets the date's day.
+	 * @param The date's new day.
+	 */
 	public void setDay(int dayIn) {
 		day = dayIn;
 	}
 
-	//turns months and years into their equivalent in days
+	/**
+	 * The method converts the months and years into their equivalent days.
+	 * @return the total days within the date.
+	 */
 	public int toDays() {
 		//instantiate vars
 		int days = day;
